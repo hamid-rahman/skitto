6515f34fb9af14e2a1c909822e19529f23f57f68
hamid-rahman <hamidz10101@gmail.com>
2022-12-02 08:00:13 +0600

    first

---
+++ dist/js/custom.js
@@ -0,0 +1,18 @@
+// hamid custom
+// AOS Initialized 
+AOS.init(
+  {duration:1500}
+);
+
+//message
+document.getElementById("popup").addEventListener("click", popupMsg);
+      
+function popupMsg() {
+  alert ("It doesn't working. Check another one.");
+}
+
+//second message
+// document.getElementsById('popupSec').addEventListener("click",secMessage);
+// function secMessage(){
+//   alert ("This item not working. Check another one.");
+// }
